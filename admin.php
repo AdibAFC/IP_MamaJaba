cb6da8fbec6d7d73ae47a674fd96fbd9085184da
Pritha Saha <149777871+PrithS24@users.noreply.github.com>
2024-07-11 16:37:49 +0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -79,7 +79,7 @@ $conn->close();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="adm.css">
+    <link rel="stylesheet" href="admin_style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
